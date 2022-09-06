09ad9637fdc0fc28fecd3148c76bfa4a0ff2754b
Lyud0k <99692275+Lyud0k@users.noreply.github.com>
2022-09-06 21:41:25 +0300

    hw10_er

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,2 +1,10 @@
-fetch('https://restcountries.com/v3.1/name/{name}');
-https://restcountries.com/v2/{service}?fields={field},{field},{field}
+
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/${name}?fields=name,capital,population,flags,languages`;
+
+    return fetch(url).then(
+        response => {
+            return response.json();
+        }
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
+const lodash = require("lodash.debounce");
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
+const findInput = document.querySelector('#search-box');
+const findDiv = document.querySelector('.country-info')
+
+findInput.addEventListener('input', lodash(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(evt) {
+  evt.preventDefault();
+  let text = evt.target.value;
+  fetchCountries(text).then(outputCountry);
+  console.log(text);
+  }
+
+function outputCountry(countries) {
+  console.log(countries);
+     const markup = countries
+    .map((country) => {
+      return `<div class="country-info">
+      <img src ="${country.flags.svg}" width="40px"/>
+          <p><b>Name</b>: ${country.name.official}</p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>
+          <p><b>Languages</b>: ${Object.values(country.languages)}</p>
+        </div>`;
+    })
+       .join("");
+  findDiv.innerHTML = markup;
+}
